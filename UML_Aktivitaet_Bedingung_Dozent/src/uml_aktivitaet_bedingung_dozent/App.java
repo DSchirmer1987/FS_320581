11ccd2057944b9bdcf9b4c37648a461ab5f7f461
DSchirmer1987 <dschirmer1987@gmail.com>
2022-04-08 11:50:16 +0200

    UML Bedingung Beispiel

---
+++ UML_Aktivitaet_Bedingung_Dozent/src/uml_aktivitaet_bedingung_dozent/App.java
@@ -0,0 +1,29 @@
+package uml_aktivitaet_bedingung_dozent;
+
+public class App {
+
+	public static void main(String[] args) {
+		int zahl = 0;
+		
+		switch(zahl) {
+			case 1: {
+				System.out.println("Zahl ist eins!");
+				break;
+			}
+			case 2: {
+				System.out.println("Zahl ist zwei!");
+				break;
+			}
+			case 3: {
+				System.out.println("Zahl ist drei!");
+				break;
+			}
+			default: {
+				System.out.println("Unbekannte Zahl!");
+				break;
+			}
+		}
+
+	}
+
+}
