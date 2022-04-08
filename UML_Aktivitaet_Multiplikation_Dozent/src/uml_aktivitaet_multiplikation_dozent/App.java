d47f9a132572ddf489da301f21906aa862649e8b
DSchirmer1987 <dschirmer1987@gmail.com>
2022-04-08 10:19:03 +0200

    UML Multiplikation

--- UML_Aktivitaet_Multiplikation_Dozent/src/uml_aktivitaet_multiplikation_dozent/App.java
+++ UML_Aktivitaet_Multiplikation_Dozent/src/uml_aktivitaet_multiplikation_dozent/App.java
@@ -3,7 +3,8 @@ package uml_aktivitaet_multiplikation_dozent;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Multiplikation multi = new Multiplikation();
+		System.out.println(multi.multipliziere(-5, 5));
 
 	}
 
